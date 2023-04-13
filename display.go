7c75b435c54acd3fbaab38bfcd9831e50645a2cf
Lionel Beato <lionel.beato@gmail.com>
2023-04-13 19:06:19 -0400

    initial commit

---
+++ display.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"image"
+	"image/color"
+
+	"gioui.org/app"
+	"gioui.org/font/gofont"
+	"gioui.org/io/system"
+	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/op/clip"
+	"gioui.org/op/paint"
+	"gioui.org/text"
+	"gioui.org/widget/material"
+)
+
+func run(w *app.Window) error {
+	th := material.NewTheme(gofont.Collection())
+	var ops op.Ops
+	for {
+		e := <-w.Events()
+		switch e := e.(type) {
+		case system.DestroyEvent:
+			return e.Err
+		case system.FrameEvent:
+			gtx := layout.NewContext(&ops, e)
+
+			defer clip.Rect{Max: image.Pt(100, 100)}.Push(&ops).Pop()
+			paint.ColorOp{Color: color.NRGBA{R: 0x80, A: 0xFF}}.Add(&ops)
+			paint.PaintOp{}.Add(&ops)
+
+			title := material.H1(th, "Hello, Gio")
+			maroon := color.NRGBA{R: 127, G: 0, B: 0, A: 255}
+			title.Color = maroon
+			title.Alignment = text.Middle
+			title.Layout(gtx)
+
+			e.Frame(gtx.Ops)
+		}
+	}
+}
