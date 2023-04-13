bb9c72f8c04ab93d3ac8197146319f4ea3eca0e4
Lionel Beato <lionel.beato@gmail.com>
2023-04-13 19:12:28 -0400

    WIP: working pixel on screen

--- display.go
+++ display.go
@@ -5,18 +5,18 @@ import (
 	"image/color"
 
 	"gioui.org/app"
-	"gioui.org/font/gofont"
+	// "gioui.org/font/gofont"
 	"gioui.org/io/system"
 	"gioui.org/layout"
 	"gioui.org/op"
 	"gioui.org/op/clip"
 	"gioui.org/op/paint"
-	"gioui.org/text"
-	"gioui.org/widget/material"
+	// "gioui.org/text"
+	// "gioui.org/widget/material"
 )
 
 func run(w *app.Window) error {
-	th := material.NewTheme(gofont.Collection())
+	// th := material.NewTheme(gofont.Collection())
 	var ops op.Ops
 	for {
 		e := <-w.Events()
@@ -25,16 +25,16 @@ func run(w *app.Window) error {
 			return e.Err
 		case system.FrameEvent:
 			gtx := layout.NewContext(&ops, e)
+			black := color.NRGBA{R: 0, G: 0, B: 0, A: 255}
 
-			defer clip.Rect{Max: image.Pt(100, 100)}.Push(&ops).Pop()
-			paint.ColorOp{Color: color.NRGBA{R: 0x80, A: 0xFF}}.Add(&ops)
+			defer clip.Rect{Max: image.Pt(10, 10)}.Push(&ops).Pop()
+			paint.ColorOp{Color: black}.Add(&ops)
 			paint.PaintOp{}.Add(&ops)
 
-			title := material.H1(th, "Hello, Gio")
-			maroon := color.NRGBA{R: 127, G: 0, B: 0, A: 255}
-			title.Color = maroon
-			title.Alignment = text.Middle
-			title.Layout(gtx)
+			// title := material.H1(th, "Hello, Gio")
+			// title.Color = maroon
+			// title.Alignment = text.Middle
+			// title.Layout(gtx)
 
 			e.Frame(gtx.Ops)
 		}
